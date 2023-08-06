a67de82046309ae1230f089213ad137e1abffc59
EdenRize <edenrize@gmail.com>
2023-08-06 18:56:46 +0300

    changes

---
+++ form/form.js
@@ -0,0 +1,7 @@
+function onSubmit(event) {
+  event.preventDefault();
+  var firstName = event.target[0].value;
+  var lastName = event.target[1].value;
+  var phone = event.target[2].value;
+  var mail = event.target[3].value;
+}
