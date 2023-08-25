18e220ad3bd819527438de822f41dd9898087e23
EdenRize <edenrize@gmail.com>
2023-08-25 17:53:10 +0300

    collect-balls completed

--- collect-balls/collect-balls.js
+++ collect-balls/collect-balls.js
@@ -1,26 +1,38 @@
 var gBoard = []; //[[],[],[],[],[]]
 var bugPos; //{i:number,j:number}
-var rowLength = 5; //number
+var rowLength = 7; //number
+const timerEl = document.querySelector(".time-count");
 const gameEl = document.querySelector(".collect-balls-container");
-var isPressed = false;
 const mainBugEl = document.querySelector(".main-bug-img");
+const blackScreenEl = document.querySelector(".black-screen");
+const startBtnEl = document.querySelector(".start-btn");
+const scoreEl = document.querySelector(".score");
+const time = "15:00";
+var isPressed = false;
+var isOver = false;
+const BEST_SCORE_KEY = "collectBallsBestScore";
 window.addEventListener("keydown", (ev) => {
+  if (isOver) return;
   if (!isPressed) {
     const key = ev.key;
     switch (key) {
       case "ArrowUp":
+      case "w":
         movePlayer("up");
         break;
 
       case "ArrowDown":
+      case "s":
         movePlayer("down");
         break;
 
       case "ArrowRight":
+      case "d":
         movePlayer("right");
         break;
 
       case "ArrowLeft":
+      case "a":
         movePlayer("left");
         break;
     }
@@ -31,19 +43,49 @@ window.addEventListener("keyup", () => {
   isPressed = false;
 });
 
+//function that starts the game
+function startGame() {
+  updateScore(true);
+  isOver = false;
+  cleanBoard();
+  blackScreenEl.hidden = true;
+  startBtnEl.hidden = true;
+  timerEl.innerText = time;
+
+  renderPlayer();
+  var counter = 0;
+  const id = setInterval(() => {
+    createEnemy();
+    counter++;
+    if (counter === rowLength) {
+      clearInterval(id);
+    }
+  }, 1000);
+  startTimer();
+}
+
+//function that cleans the board
+function cleanBoard() {
+  const children = gameEl.children;
+
+  for (let i = 0; i < children.length; i++) {
+    if (children[i].innerHTML) {
+      children[i].removeChild(children[i].children[0]);
+    }
+  }
+  const mainBugPlace = Math.ceil(rowLength / 2 - 1);
+  bugPos = { i: mainBugPlace, j: mainBugPlace };
+}
+
 //function that renders the board
 function renderBoard() {
+  timerEl.innerText = time;
   const containerEl = document.querySelector(".collect-balls-container");
   containerEl.style.gridTemplateColumns = `repeat(${rowLength}, 1fr)`;
   for (let i = 0; i < rowLength; i++) {
     for (let j = 0; j < rowLength; j++) {
       gameEl.innerHTML += `<div data-cell={"i":${i},"j":${j}} class="square"></div>`;
     }
-  }
-  //   console.log(JSON.parse(document.querySelector("[data-cell]").dataset.cell));
-  renderPlayer();
-  for (let i = 0; i < rowLength; i++) {
-    createEnemy();
   }
 }
 
@@ -62,8 +104,34 @@ function buildGborad() {
   gBoard[mainBugPlace][mainBugPlace].content = "main bug";
   bugPos = { i: mainBugPlace, j: mainBugPlace };
 
-  console.table(gBoard);
   renderBoard();
+}
+
+//function that starts the timer
+function startTimer() {
+  var currTime = parseInt(timerEl.innerText);
+  const timerId = setInterval(() => {
+    currTime = (currTime - 0.01).toFixed(2);
+    timerEl.innerText = currTime;
+    if (Math.ceil(currTime) === 5) {
+      timerEl.style.color = "red";
+    }
+    if (Math.ceil(currTime) === 0) {
+      endGame();
+      clearInterval(timerId);
+    }
+  }, 10);
+}
+
+//function that ends the game
+function endGame() {
+  updateBestScore();
+  isOver = true;
+  const playAgainBtnEl = document.querySelector(".play-again-btn");
+  const endMsgEl = document.querySelector(".end-msg");
+  blackScreenEl.hidden = false;
+  playAgainBtnEl.hidden = false;
+  endMsgEl.hidden = false;
 }
 
 //function that moves the player accourding to arrows
@@ -97,7 +165,6 @@ function movePlayer(direction) {
       }
       break;
   }
-  console.table(gBoard);
 }
 
 //function that updates the player on screen
@@ -108,7 +175,6 @@ function updatePos(position) {
   gBoard[newPos.i][newPos.j].content = "main bug";
   bugPos = newPos;
   renderPlayer();
-  console.table(gBoard);
 }
 
 //function that renders the player in the new pos
@@ -179,7 +245,6 @@ function eatEnemy(position) {
       position.j === currPosition.j &&
       children[i].innerHTML
     ) {
-      console.log(children[i]);
       children[i].removeChild(children[i].children[0]);
       updateScore();
       setTimeout(() => {
@@ -191,12 +256,41 @@ function eatEnemy(position) {
 }
 
 //function that updates the score
-function updateScore() {
-  const scoreEl = document.querySelector(".score");
+function updateScore(isReset) {
+  if (isReset) {
+    scoreEl.innerText = 0;
+    return;
+  }
   scoreEl.innerText = +scoreEl.innerText + 1;
+}
+
+//function that checks best score, and updates it to the div
+function updateBestScore() {
+  const bestScoreEl = document.querySelector(".best-score");
+  const bestScore = getLocalStorage(BEST_SCORE_KEY);
+  const currScore = +scoreEl.innerText;
+  if (bestScore) {
+    if (currScore > bestScore) {
+      insertLocalStorage(BEST_SCORE_KEY, currScore);
+    }
+  } else {
+    insertLocalStorage(BEST_SCORE_KEY, currScore);
+  }
+
+  bestScoreEl.innerText = getLocalStorage(BEST_SCORE_KEY);
 }
 
 // function that give a random number between min and max (not including max)
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//function that inserts to local storage
+function insertLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+//function that gets item from local storage
+function getLocalStorage(key) {
+  return localStorage.getItem(key);
 }
